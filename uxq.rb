b04d9eec4f0faf17873442e48cc46d449d85b5c0
Trent McClenahan <trent.mcclenahan@gmail.com>
2016-06-02 23:27:51 +1000

    Caching requests

--- uxq.rb
+++ uxq.rb
@@ -1,15 +1,22 @@
 require 'sinatra'
 require 'twitter'
 
-client = Twitter::REST::Client.new do |config|
+@@client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
 end
 
+@@tweets = {}
+
 get '/tag/:tag' do
-  client.search("##{params['tag']}", result_type: "recent").take(1).collect do |tweet|
-    "#{tweet.user.screen_name}: #{tweet.text}"
-  end
+  tag = params['tag']
+  refresh(tag) if @@tweets[tag] == nil
+  t = @@tweets[tag].sample
+  "<h1>#{t.user.screen_name}: #{t.text}</h1>"
+end
+
+def refresh(tag)
+  @@tweets[tag] = @@client.search("##{tag}", result_type: "mixed", count: 50)
 end
