a1a3586b1c670f14f09ab43c374ec532e5c9ee99
Trent McClenahan <trent.mcclenahan@gmail.com>
2016-06-02 23:47:33 +1000

    Trying again

--- uxq.rb
+++ uxq.rb
@@ -1,22 +1,32 @@
 require 'sinatra'
 require 'twitter'
 
-@@client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-end
+class UxqTwitter
+  @@client = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
 
-@@tweets = {}
+  @@tweets = {}
+
+  def self.client
+    @@client
+  end
+
+  def self.tweets
+    @@tweets
+  end
+end
 
 get '/tag/:tag' do
   tag = params['tag']
-  refresh(tag) if @@tweets[tag] == nil
-  t = @@tweets[tag].sample
+  refresh(tag) if UxqTwitter.tweets[tag] == nil
+  t = UxqTwitter.tweets[tag].sample
   "<h1>#{t.user.screen_name}: #{t.text}</h1>"
 end
 
 def refresh(tag)
-  @@tweets[tag] = @@client.search("##{tag}", result_type: "mixed", count: 50)
+  UxqTwitter.tweets[tag] = UxqTwitter.client.search("##{tag}", result_type: "mixed", count: 15).to_a
 end
